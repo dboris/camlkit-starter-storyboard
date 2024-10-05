b56f13a22cd109f84f4759df58aefaf0a3774861
Boris D. <boris.dob@gmail.com>
2024-10-05 16:52:05 -0700

    Initial commit.

---
+++ CamlLib/CamlLib.ml
@@ -0,0 +1,136 @@
+open Foundation
+open UIKit
+open Runtime
+
+let greetings =
+  [| "English", "Hello World!"
+   ; "Spanish", "Hola Mundo!"
+   |]
+
+module GreetingsTVC = struct
+  module LLong = Objc.LLong
+
+  (* Prototype cell for this ID is registered in Main.xib *)
+  let cellID = new_string "Cell"
+
+  let numberOfSectionsInTableView =
+    Method.define
+      ~args: Objc_t.[id]
+      ~return: Objc_t.llong
+      ~cmd: (selector "numberOfSectionsInTableView:")
+      (fun _self _cmd _tv -> LLong.of_int 1)
+
+  let titleForHeaderInSection =
+    Method.define
+      ~args: Objc_t.[id; llong]
+      ~return: Objc_t.id
+      ~cmd: (selector "tableView:titleForHeaderInSection:")
+      (fun _self _cmd _tv _section -> new_string "Language")
+
+  let numberOfRowsInSection =
+    Method.define
+      ~args: Objc_t.[id; llong]
+      ~return: Objc_t.llong
+      ~cmd: (selector "tableView:numberOfRowsInSection:")
+      (fun _self _cmd _tv _section -> LLong.of_int (Array.length greetings))
+
+  let cellForRowAtIndexPath =
+    Method.define
+      ~args: Objc_t.[id; id]
+      ~return: Objc_t.id
+      ~cmd: (selector "tableView:cellForRowAtIndexPath:")
+      (fun _self _cmd tv index_path ->
+        let cell =
+          tv |> UITableView.dequeueReusableCellWithIdentifier' cellID
+            ~forIndexPath: index_path
+        and i = index_path |> NSIndexPath.row |> LLong.to_int in
+        cell
+        |> UITableViewCell.textLabel
+        |> UILabel.setText (new_string (fst greetings.(i)));
+        cell)
+
+  let didSelectRowAtIndexPath =
+    Method.define
+      ~args: Objc_t.[id; id]
+      ~return: Objc_t.void
+      ~cmd: (selector "tableView:didSelectRowAtIndexPath:")
+      (fun self _cmd _tv index_path ->
+        let i = index_path |> NSIndexPath.row |> LLong.to_int in
+        self
+        |> UIViewController.parentViewController  (* nav vc *)
+        |> UIViewController.parentViewController  (* split vc *)
+        |> UISplitViewController.viewControllerForColumn
+            _UISplitViewControllerColumnSecondary
+        |> UIViewController.view
+        |> UIView.viewWithTag 1
+        |> UILabel.setText (new_string (snd greetings.(i))))
+
+  let viewDidLoad =
+    Method.define
+      ~args: Objc_t.[]
+      ~return: Objc_t.void
+      ~cmd: (selector "viewDidLoad")
+      (fun self cmd ->
+        self |> msg_super cmd ~args: Objc_t.[] ~return: Objc_t.void;
+        self |> UIViewController.setTitle (new_string "Greetings"))
+
+  (* This class is referenced in Main.xib *)
+  let _self =
+    Class.define "GreetingsTVC"
+      ~superclass: UITableViewController.self
+      ~methods:
+        [ numberOfSectionsInTableView
+        ; titleForHeaderInSection
+        ; numberOfRowsInSection
+        ; cellForRowAtIndexPath
+        ; didSelectRowAtIndexPath
+        ; viewDidLoad
+        ]
+end
+
+module SceneDelegate = struct
+  (* This class is referenced in Info.plist, UISceneConfigurations key.
+    It is instantiated from UIApplicationMain. *)
+  let _self =
+    Class.define "SceneDelegate"
+      ~superclass: UIResponder.self
+      ~protocols: [Objc.get_protocol "UIWindowSceneDelegate"]
+      ~ivars: [Ivar.define "window" Objc_t.id]
+      ~methods: (Property._object_ "window" Objc_t.id ())
+end
+
+module AppDelegate = struct
+  (* This class is referenced in main.m. It is instantiated from UIApplicationMain. *)
+  let _self =
+    Class.define "AppDelegate"
+      ~superclass: UIResponder.self
+      ~methods:
+        [ Method.define
+          ~cmd: (selector "application:didFinishLaunchingWithOptions:")
+          ~args: Objc_t.[id; id]
+          ~return: Objc_t.bool
+          (fun self _cmd _app _opts ->
+            NSNotificationCenter.self
+            |> NSNotificationCenterClass.defaultCenter
+            |> NSNotificationCenter.addObserver self
+              ~selector_: (selector "sceneActivated")
+              ~name: _UISceneDidActivateNotification
+              ~object_: nil;
+            true)
+
+        ; Method.define
+          ~cmd: (selector "sceneActivated")
+          ~args: Objc_t.[id]
+          ~return: Objc_t.void
+          (fun _self _cmd _scene -> Printf.eprintf "sceneActivated...\n%!")
+
+        ; Method.define
+          ~cmd: (selector "application:configurationForConnectingSceneSession:options:")
+          ~args: Objc_t.[id; id; id]
+          ~return: Objc_t.id
+          (fun _self _cmd _app conn_session _opts ->
+            alloc UISceneConfiguration.self
+            |> UISceneConfiguration.initWithName (new_string "Default Configuration")
+                ~sessionRole: (UISceneSession.role conn_session))
+        ]
+end
---
+++ README.md
@@ -0,0 +1,73 @@
+## Overview
+
+This is a storyboard-based starter template for an iOS application built with
+[Camlkit](https://github.com/dboris/camlkit) in OCaml. The template features
+a minimal Xcode project useful for testing the application on an iOS
+simulator, installing it on a device, debugging, etc.
+
+The bulk of the application is built in OCaml and packaged as an _xcframework_,
+which gets linked with the Xcode project. The application follows the
+[Main program in C](https://ocaml.org/manual/5.2/intfc.html#ss:main-c) pattern,
+and the [`main` function](https://github.com/dboris/camlkit-starter-storyboard/blob/master/CamlApp/main.m)
+is very minimal:
+
+```c
+int main(int argc, char * argv[]) {
+    caml_startup(argv);
+    return UIApplicationMain(argc, argv, nil, @"AppDelegate");
+}
+```
+
+The call to `caml_startup` initializes the OCaml runtime and causes the
+evaluation of [the OCaml code](https://github.com/dboris/camlkit-starter-storyboard/blob/master/CamlLib/CamlLib.ml).
+As a result of this evaluation, three Objective-C classes are defined and
+registered with the Objective-C runtime: `GreetingsTVC`, `SceneDelegate`,
+and `AppDelegate`.
+
+The next call is to `UIApplicationMain`, which doesn't exit for the duration
+of running the application. `UIApplicationMain` instantiates UIApplication
+and `AppDelegate`, and when appropriate calls back the delegate's
+`application:didFinishLaunchingWithOptions:` method. This is the
+entry point in the OCaml part of the application, suitable for performing
+global initialization tasks.
+
+`UIApplicationMain` creates a UISceneSession, a UIWindowScene, and an
+instance that will serve as the window scene’s delegate. The class of the
+window scene’s delegate is specified in [`Info.plist`](https://github.com/dboris/camlkit-starter-storyboard/blob/master/CamlApp/Info.plist),
+under the key _UISceneDelegateClassName_.
+
+`UIApplicationMain` looks to see whether the initial scene uses a storyboard.
+`Info.plist` specifies the name of its storyboard under the key path:
+  _UIApplicationSceneManifest_ -> _UISceneConfigurations_ -> _UISceneStoryboardFile_
+
+`UIApplicationMain` instantiates that storyboard’s initial view controller,
+a UIWindow, and assigns the initial view controller instance to the window’s
+`rootViewController` property. `UIApplicationMain` causes the app’s
+interface to appear by calling the UIWindow instance method `makeKeyAndVisible`.
+
+## Setup
+
+```sh
+opam repository add ios https://github.com/ocaml-cross/opam-cross-ios.git
+
+opam switch create 4.14.2-device 4.14.2 --repos=default,ios
+eval $(opam env)
+ARCH=arm64 SUBARCH=arm64 PLATFORM=iPhoneOS SDK=$(xcrun --sdk iphoneos --show-sdk-version) VER=13.0 opam install conf-ios
+opam install camlkit-ios
+
+opam switch create 4.14.2-simulator 4.14.2 --repos=default,ios
+eval $(opam env)
+opam install conf-simulator-ios
+ARCH=amd64 SUBARCH=x86_64 PLATFORM=iPhoneSimulator SDK=$(xcrun --sdk iphonesimulator --show-sdk-version) VER=13.0 opam install conf-ios
+opam install camlkit-ios
+
+opam switch create 4.14.2-simulator-arm 4.14.2 --repos=default,ios
+eval $(opam env)
+opam install conf-simulator-ios
+ARCH=arm64 SUBARCH=arm64 PLATFORM=iPhoneSimulator SDK=$(xcrun --sdk iphonesimulator --show-sdk-version) VER=13.0 opam install conf-ios
+opam install camlkit-ios
+```
+
+## Building
+
+Use `make` to build the _xcframework_ and `make open` to open the project in Xcode.
